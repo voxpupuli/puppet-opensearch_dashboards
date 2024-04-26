6500e91514d3fcf149ed8ff8b0e7cb0a86932371
Romain Tartière <romain@blogreen.org>
2024-04-25 21:57:15 -1000

    Fix acceptance tests
    The configuration directory is not managed by the module.  Permissions
    used to be too relaxed by default and where fixed upsteam.  We should
    not check the unmanaged directory permissions, owner and group. The
    configuration file itself, in this directory, is already tested by the
    module.

--- spec/acceptance/default_spec.rb
+++ spec/acceptance/default_spec.rb
@@ -19,12 +19,6 @@ describe 'opensearch_dashboards' do
       it { is_expected.to be_running }
     end
 
-    describe file('/etc/opensearch-dashboards') do
-      it { is_expected.to be_directory }
-      it { is_expected.to be_owned_by 'opensearch-dashboards' }
-      it { is_expected.to be_grouped_into 'opensearch-dashboards' }
-    end
-
     describe file('/etc/opensearch-dashboards/opensearch_dashboards.yml') do
       it { is_expected.to be_file }
       it { is_expected.to be_owned_by 'opensearch-dashboards' }
